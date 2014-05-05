20256f61a6acb374219045bc3b02fb6a4e868cf4
--global <amelia.f.antrim.14@dartmouth.edu>
2014-05-04 23:07:16 -0400

    updating precision-recall

--- src/SequentialPairs/Statistics.py
+++ src/SequentialPairs/Statistics.py
@@ -1,19 +1,41 @@
-#!/Users/ameliaantrim/anaconda/bin/python
+#!/Users/student/anaconda/bin/python
+'''
+.. module:: Statistics
+   :platform: Unix, Windows
+   :synopsis: Generates statistics about results. 
+
+.. moduleauthor:: Amelia F. Antrim <amelia.f.antrim@gmail.com>
+
+'''
+
 
 import sys
 import os
 
 class Statistics:
-    def __init__(self, predicted_contacts, true_contacts):
-        self.positives(predicted_contacts, true_contacts)
+    '''
+    Generates lists of true and false positives/negatives given predictions and true answers.
+    
+    :param predicted_contacts: estimated positives
+    :type predicted_contacts: array
+    :param true_contacts: list of true positives (whether predicted or not)
+    :type true_contacts: array
+    :param antigen_chain: antigen structure
+    :type antigen_chain: Bio.PDB.Chain object. 
+    
+    '''
+    def __init__(self, predicted_contacts, true_contacts, antigen_chain):
+        self.positives(predicted_contacts, true_contacts, antigen_chain)
 
-    def positives(self, predicted_contacts, true_contacts):
+    def positives(self, predicted_contacts, true_contacts, antigen_chain):
         # Make a hash table to check the validity of positives
         positives = {} # Doesn't need to be a self b/c only used internally for validation
         self.true_positives = []
         self.false_positives = []
-        self.true_negatives = []
-
+        self.false_negatives = []
+        
+        print "true contacts"
+        print true_contacts
         # Add all the true contact pairs to the hash table
         # Using a 0/1 flag to indicate whether the residue was found or not
         for residue in true_contacts:
@@ -24,17 +46,21 @@ class Statistics:
             # If the predicted positive is in the true positive hash
             if residue in positives.keys():
                 # Mark it as estimated 
+                #print "\nTRUE: Comparing " + str(residue) + " and " + str(positives[residue])
                 self.true_positives.append(residue)
                 # Change the flag to "discovered
                 positives[residue] += 1
             # Else it is a false positive
             else:
                 self.false_positives.append(residue)
+                #print "\nFALSE: Comparing " + str(residue)
 
         # Now check whether the true positives were predicted or not
         for residue in positives.iterkeys():
             if positives[residue] == 0:
                 self.false_negatives.append(residue)
+        
+        return self.true_positives, self.false_positives
 
         # If we want to find true negatives, we would iterate through the entire structure
         # and record everything that wasn't already in any of these categories
